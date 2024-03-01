d39e008f1e63647d2cc6dd87d5d7b10f2863ed27
Al Ameen <caalameen123@gmail.com>
2024-03-01 11:50:30 +0530

    sleepIn completed

---
+++ codingbat/warmup1/sleepIn.java
@@ -0,0 +1,17 @@
+package warmup1;
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    if(weekday == false && vacation == false)
+    {
+      return true;
+    }
+    else if(weekday == true && vacation == false)
+    {
+      return false;
+    }
+    else
+    {
+     return true; 
+    }
+    
+  }
+  
