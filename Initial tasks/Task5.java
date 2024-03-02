9377a7868cf254bc1f52b350d9996b96b19bd8b5
Al Ameen <caalameen123@gmail.com>
2024-03-02 12:25:43 +0530

    Task5 completed

---
+++ Initial tasks/Task5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) 
+    {
+        double a, b, c, x;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a:");
+        a = sc.nextInt();
+        System.out.print("Enter b:");
+        b = sc.nextInt();
+        System.out.print("Enter c:");
+        c = sc.nextInt();
+        sc.close();
+
+
+        if (b * b - 4 * a * c == 0) 
+        {   
+            x = -b / (2 * a);
+            System.out.printf("The value of x is:%.0f", x);
+        } else if (b * b - 4 * a * c > 0) 
+        {
+            x = (-b + Math.sqrt(b*b - 5 * a * c)) / (2 * a);
+            System.out.printf("The value of x is: %.0f%n", x);
+            x = (-b - Math.sqrt(b*b - 5 * a * c)) / (2 * a);
+            System.out.printf("The value of x is: %.0f", x);
+        } else
+            System.out.print("The value of x is undefined");
+    }
+}
