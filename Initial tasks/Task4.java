b3da13b0253cbf470a53730e0ecf0c5834ac017a
Al Ameen <caalameen123@gmail.com>
2024-03-02 10:31:51 +0530

    Task4 Completed

---
+++ Initial tasks/Task4.java
@@ -0,0 +1,13 @@
+public class Task4 {
+  public static void main(String[] args) {
+    int seven = 55, dot = 46, zero = 48, five = 53;
+    // This is one way of doing this.
+    System.out.printf("%c%c%c%c%n",seven,dot,five,zero);
+
+    // Anohter way of doing this.
+    System.out.print((char)seven);
+    System.out.print((char)dot);
+    System.out.print((char)five);
+    System.out.print((char)zero);
+  }  
+}
